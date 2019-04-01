1ee739b2d1ffe66750c95f1d046b5d2fbf83793b
lafetamarcelo <marcelolafeta.nsee@gmail.com>
2019-04-01 18:58:17 -0300

    Web Hook locally working.

--- rest_api/code/app.py
+++ rest_api/code/app.py
@@ -1,22 +1,29 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from matchable_observable import MOLIwhiteBox
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
-toDos = {}
-
 class ToDoSimple(Resource):
-    def get(self, toDo_id):
-        send = int(toDos[toDo_id]) * 2
-        return {toDo_id: send}
+    def get(self, model_id):
+        samples = {'time': request.form['time'],
+                   'levAl': request.form['levAl'],
+                   'windDecisions': request.form['windDecisions']}
+        return samples
 
-    def put(self, toDo_id):
-        toDos[toDo_id] = request.form['data']
-        return {toDo_id: toDos[toDo_id]}
+    def put(self, model_id):
+        samples = {'time': json.loads(request.form['time']),
+                   'levAl': json.loads(request.form['levAl']),
+                   'windDecisions': json.loads(request.form['windDecisions'])}
+        modelMOLI = MOLIwhiteBox('trivial')
+        modelMOLI.fit(samples)
+        return {'omega': modelMOLI.tau}
+        #toDos[toDo_id] = request.form['data']
+        #return {toDo_id: toDos[toDo_id]}
 
-api.add_resource(ToDoSimple, '/toDos/<string:toDo_id>')
+api.add_resource(ToDoSimple, '/<string:model_id>')
 
 if __name__ == '__main__':
     app.run(port=5000, debug = True)
